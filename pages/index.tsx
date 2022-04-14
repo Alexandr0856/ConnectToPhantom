1dc6e5ff8f9d6fd392ce5a1ec0c4a71d5e722363
Alexandr0856 <ppos19soo@gmail.com>
2022-04-14 20:41:31 +0200

    Upload

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Connect from "../components/ConnectToPhantom";
+
+
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <Connect />
+    </div>
+  );
+}
